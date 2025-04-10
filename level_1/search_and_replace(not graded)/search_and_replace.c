97ed00ed01807e301161d390dc67e72747ed902a
alicigar <aliciamomoko@gmail.com>
2025-04-10 22:44:11 +0200

    approved by grademe

--- level_1/search_and_replace(not graded)/search_and_replace.c
+++ level_1/search_and_replace(not graded)/search_and_replace.c
@@ -1,20 +1,19 @@
 #include <unistd.h>
 
-void    search_and_replace(char *s, char old, char new)
+void	search_and_replace(char *s, char *old_char, char *new_char)
 {
-    while(*s)
-    {
-        if (*s == old)
-            *s = new;
-        write (1, s, 1);
-        s++;
-    }
+	while (*s)
+	{
+		if (*s == *old_char)
+			*s = *new_char;
+		write (1, s, 1);
+		s++;
+	}
 }
 
-int main (int argc, char **argv)
+int	main (int argc, char **argv)
 {
-    if (argc == 4 & !argv[2][1] && !argv[3][1])
-            search_and_replace(argv[1], argv[2][0], argv[3][0]);
-    write (1, "\n", 1);
-    return (0);
+	if (argc == 4 && (!(argv[2][1] || argv[3][1])))
+		search_and_replace(argv[1], argv[2], argv[3]);
+	write (1, "\n", 1);
 }
