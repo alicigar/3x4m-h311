d530771ef048a98eadc4160c35b8161868a79f6f
alicigar <aliciamomoko@gmail.com>
2025-04-10 20:19:29 +0200

    Update search_and_replace.c

--- level_1/search_and_replace(not graded)/search_and_replace.c
+++ level_1/search_and_replace(not graded)/search_and_replace.c
@@ -15,7 +15,6 @@ int main (int argc, char **argv)
 {
     if (argc == 4 & !argv[2][1] && !argv[3][1])
             search_and_replace(argv[1], argv[2][0], argv[3][0]);
-    else
-            write (1, "\n", 1);
+    write (1, "\n", 1);
     return (0);
 }
