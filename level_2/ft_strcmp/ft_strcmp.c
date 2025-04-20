21c9b5c5b8d7d24436c77e08f35b4058f1a21479
alicigar <aliciamomoko@gmail.com>
2025-04-20 21:29:12 +0200

    Add files via upload
    ~

---
+++ level_2/ft_strcmp/ft_strcmp.c
@@ -0,0 +1,25 @@
+int    ft_strcmp(char *s1, char *s2)
+{
+    int i;
+
+    i = 0;
+    while(s1[i] && s2[i])
+    {
+        if (s1[i] != s2[i])
+            break ;
+        i++;
+    }
+    return (s1[i] - s2[i]);
+}
+
+/*#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char *str1 = "Hellfqefeafa";
+    char *str2 = "Helle";
+    printf("%d\n", ft_strcmp(str1, str2));
+    printf("%d\n", strcmp(str1, str2));
+    return (0);
+}*/
