d91ef40e56419462b732738840d32b1be79da386
alicigar <aliciamomoko@gmail.com>
2025-04-20 19:24:51 +0200

    success

---
+++ level_3/tab_mult/tab_mult.c
@@ -0,0 +1,52 @@
+#include <unistd.h>
+
+int ft_atoi (char *str)
+{
+    int n;
+
+    n = 0;
+    while (*str >= '0' && *str <= '9')
+    {
+        n = n * 10 + (*str - '0');
+        str++;
+    }
+    return (n);  
+}
+
+void ft_putnbr(int num)
+{
+    if (num > 9)
+        ft_putnbr(num / 10);
+    write(1, &"0123456789"[num % 10], 1);
+}
+
+void    tab_mult (char *str)
+{
+    int n;
+    int i;
+    int r;
+
+    i = 1;
+    n = ft_atoi(str);
+    r = 0;
+    while (i <= 9)
+    {
+        ft_putnbr(i);
+        write(1, " x ", 3);
+        ft_putnbr(n);
+        write(1, " = ", 3);
+        r = i * n;
+        ft_putnbr(r);
+        i++;
+        write(1, "\n", 1);
+    }
+}
+
+int main (int argc, char **argv)
+{
+    if (argc == 2)
+        tab_mult(argv[1]);
+    else
+        write (1, "\n", 1);
+    return (0);
+}
