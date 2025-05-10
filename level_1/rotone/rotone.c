b69acc2718fc353f490643e64f530861a39c7e38
alicigar <aliciamomoko@gmail.com>
2025-05-10 20:48:58 +0200

    updated

--- level_1/rotone/rotone.c
+++ level_1/rotone/rotone.c
@@ -14,25 +14,24 @@
 
 void	rotone(char *str)
 {
-	while (*str)
+	int	i;
+
+	i = 0;
+	while (str[i])
 	{
-		if ((*str >= 'a' && *str <= 'y') || (*str >= 'A' && *str <= 'Y'))
-			*str = *str + 1;
-		else if (*str == 'z')
-			*str = ('a');
-		else if (*str == 'Z')
-			*str = ('A');
-		write (1, str, 1);
-		str++;
+		if ((str[i] >= 'a' && str[i] <= 'y') || (str[i] >= 'A' && str[i] <= 'Y'))
+			str[i] = str[i] + 1;
+		else if (str[i] == 'z' || str[i] == 'Z')
+			str[i] = str[i] - 25;
+		write (1, &str[i], 1);
+		i++;
 	}
-	write (1, "\n", 1);
 }
 
 int	main(int argc, char **argv)
 {
-	if (argc != 2)
-		write (1, "\n", 1);
-	else
+	if (argc == 2)
 		rotone(argv[1]);
+	write (1, "\n", 1);	
 	return (0);
 }
