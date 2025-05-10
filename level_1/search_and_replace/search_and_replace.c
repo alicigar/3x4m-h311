67779a3c9b7ff32bf77123e6be905fe18a5acdd7
alicigar <aliciamomoko@gmail.com>
2025-05-10 21:05:29 +0200

    Add files via upload

---
+++ level_1/search_and_replace/search_and_replace.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void	search_and_replace(char *s, char *old_char, char *new_char)
+{
+	while (*s)
+	{
+		if (*s == *old_char)
+			*s = *new_char;
+		write (1, s, 1);
+		s++;
+	}
+}
+
+int	main (int argc, char **argv)
+{
+	if (argc == 4 && !argv[2][1] && !argv[3][1])
+		search_and_replace(argv[1], argv[2], argv[3]);
+	write (1, "\n", 1);
+}
