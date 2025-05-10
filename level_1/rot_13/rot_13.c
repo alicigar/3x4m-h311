9c39fd010448f4af18bd7b014e6a73587e23142e
alicigar <aliciamomoko@gmail.com>
2025-05-10 20:43:08 +0200

    updated

--- level_1/rot_13/rot_13.c
+++ level_1/rot_13/rot_13.c
@@ -2,26 +2,35 @@
 
 void rot_13(char *str)
 {
-    while (*str)
+    int    i;
+
+    i = 0;
+    while (str[i])
     {
-        if (*str >= 65 && *str <= 77)
-            *str = *str + 13;
-        else if(*str >= 78 && *str <= 90)
-            *str = *str - 13;
-        if (*str >= 97 && *str <= 109)
-            *str = *str + 13;
-        else if(*str >= 110 && *str <= 122)
-            *str = *str - 13;
-        write (1, str, 1);
-        str++;
+        if ((str[i] >= 'A' && str[i] <= 'M') || (str[i] >= 'a' && str[i] <= 'm'))
+        {
+            str[i] = str[i] + 13;
+            write(1, &str[i], 1);
+            i++;
+        }
+        else if((str[i] >= 'N' && str[i] <= 'Z') || str[i] >= 'n' && str[i] <= 'z'))
+        {
+            str[i] = str[i] - 13;
+            write(1, &str[i], 1);
+            i++;
+        }
+        else
+        {
+            write (1, &str[i], 1);
+            i++;
+        }
     }
-    write (1, "\n", 1);
 }
+
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-        write(1, "\n", 1);
-    else
+    if (argc == 2)
         rot_13(argv[1]);
+     write(1, "\n", 1);
     return (0);
 }
