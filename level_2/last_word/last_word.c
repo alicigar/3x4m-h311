f3b5927ee64474e51d961470c7e8b361cf1875ec
alicigar <aliciamomoko@gmail.com>
2025-05-10 21:26:04 +0200

    updated

--- level_2/last_word/last_word.c
+++ level_2/last_word/last_word.c
@@ -12,21 +12,22 @@
 
 #include <unistd.h>
 
-void	last_word(char *s)
+void	last_word(char *str)
 {
 	int	i;
 	
 	i = 0;
-	while (s[i])
+	while (str[i])
 		i++;
-	while (s[i] < 33)
+	i--;
+	while (str[i] == 9 || str[i] == 32)
 		i--;
-	while (s[i] > 33)
+	while (str[i] && str[i] > 32)
 		i--;
 	i++;
-	while (s[i] > 33)
+	while (str[i] && str[i] > 32)
 	{
-		write (1, &s[i], 1);
+		write (1, &str[i], 1);
 		i++;
 	}
 	
