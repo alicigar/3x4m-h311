20dc4b27348ca0f37a60a9121fd095646157b95f
alicigar <aliciamomoko@gmail.com>
2025-05-10 20:19:04 +0200

    updated

--- level_1/first_word/first_word.c
+++ level_1/first_word/first_word.c
@@ -7,19 +7,17 @@ void first_word(char *str)
     i = 0;
     while (str[i] == 9 || str[i] == 32)
         i++;
-    while (str[i] != '\0' && str[i] != 9 && str[i] != 32)
+    while (str[i] && str[i] > 32)
     {
         write (1, &str[i], 1);
         i++;
     }
-    write (1, "\n", 1);
 }
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-        write (1, "\n", 1);
-    else
+    if (argc == 2)
         first_word(argv[1]);
+    write(1, "\n", 1);
     return (0);
 }
