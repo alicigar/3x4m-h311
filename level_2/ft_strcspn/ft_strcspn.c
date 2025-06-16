0d2a9212ff5cda651a84add3b428e1acfa33d4f6
alicigar <aliciamomoko@gmail.com>
2025-06-16 20:03:33 +0200

    ok

---
+++ level_2/ft_strcspn/ft_strcspn.c
@@ -0,0 +1,34 @@
+/*#include <stdio.h>*/
+#include <string.h>
+
+size_t  ft_strcspn(const char *s, const char *reject)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (s[i])
+	{
+		j = 0;
+		while (reject[j])
+		{
+			if (reject[j] == s[i])
+				return (i);
+			j++;
+		}
+		i++;
+	}
+	return (i);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	printf("%lu\n", ft_strcspn("hello123", "0123456789"));
+	printf("%lu\n", strcspn("hello123", "0123456789"));
+}*/
+
+/*ejemplo: si *s es "hello123" y *reject es "0123456789", devuelve (i) que es 5.
+Básicamente, cuenta los caracteres de *s hasta encontrar uno que aparezca en *reject.
+Si no hay coincidencias, recorre *s y cuenta lo que mide.*/
