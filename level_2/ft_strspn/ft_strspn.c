12634184de55473d260c49f0ee35b7252f7f58be
alicigar <aliciamomoko@gmail.com>
2025-06-16 20:05:07 +0200

    ok

---
+++ level_2/ft_strspn/ft_strspn.c
@@ -0,0 +1,32 @@
+#include <string.h>
+
+size_t	ft_strspn(const char *s, const char *accept)
+{
+	size_t	i;
+	size_t	j;
+	
+	i = 0;
+	while (s[i])
+	{
+		j = 0;
+		while (accept[j])
+		{
+			if (accept[j] == s[i])
+				break;
+			j++;
+		}
+		if (!accept[j])
+			return(i);
+		i++;
+	}
+	return (i);
+}
+
+/*#include <stdio.h>
+
+int	main(void)
+{
+	printf("%lu\n", ft_strspn("hello123", "helo"));
+	printf("%lu\n", strspn("hello123", "helo"));
+}*/
+/*Ejemplo: si *s es "hello123" y *accept "helo", devuelve (i) que es "5"*/
