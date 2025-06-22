006b30c5682585f9864147604b2be0e49d6a655a
alicigar <aliciamomoko@gmail.com>
2025-06-23 00:33:28 +0200

    updated

--- level_1/rot_13/rot_13.c
+++ level_1/rot_13/rot_13.c
@@ -8,22 +8,12 @@ void rot_13(char *str)
     while (str[i])
     {
         if ((str[i] >= 'A' && str[i] <= 'M') || (str[i] >= 'a' && str[i] <= 'm'))
-        {
             str[i] = str[i] + 13;
-            write(1, &str[i], 1);
-            i++;
-        }
         else if((str[i] >= 'N' && str[i] <= 'Z') || str[i] >= 'n' && str[i] <= 'z'))
-        {
             str[i] = str[i] - 13;
-            write(1, &str[i], 1);
-            i++;
-        }
-        else
-        {
-            write (1, &str[i], 1);
-            i++;
-        }
+      
+        write (1, &str[i], 1);
+        i++;
     }
 }
 
