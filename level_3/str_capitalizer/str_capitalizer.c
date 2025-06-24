3f0393d767fe4001b42773cf110dbe1b1ff70481
alicigar <aliciamomoko@gmail.com>
2025-06-24 22:33:58 +0200

    ok

--- level_3/str_capitalizer/str_capitalizer.c
+++ level_3/str_capitalizer/str_capitalizer.c
@@ -1,40 +1,31 @@
 #include <unistd.h>
 
-void    str_capitalizer(char *str)
+void	str_capitalizer(char *str)
 {
-    int i = 0;
-    
-    while (str[i])
-    {
-        if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t')
-        {
-            if (str[i] >= 'a' && str[i] <= 'z')
-                str[i] -= 32; 
-        }
-        else
-        {
-            if (str[i] >= 'A' && str[i] <= 'Z')
-                str[i] += 32;
-        }
-        write(1, &str[i], 1);
-        i++;
-    }
+	int	i = 0;
+	
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] + 32;
+		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i - 1] == 9 || str[i - 1] == 32|| str[i -1] == 0))
+			str[i] = str[i] - 32;
+		write(1, &str[i], 1);
+		i++;
+	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int i;
+	int	i = 1;
+	
+	if (argc == i)
+		write(1, "\n", 1);
 
-    i = 1;
-    if (argc == i)
-        write (1, "\n", 1);
-    else
-    {
-        while (i < argc)
-        {
-            str_capitalizer(argv[i]);
-            write(1, "\n", 1);
-            i++;
+	while (argc > i)
+	{
+		str_capitalizer(argv[i]);
+		write(1, "\n", 1);
+		i++;
 	}
-    }
 }
