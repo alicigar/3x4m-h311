a0f1a56eddd04a61af1915a6c876b986cb1e591f
alicigar <aliciamomoko@gmail.com>
2025-06-24 17:17:53 +0200

    now ok

--- level_4/ft_itoa/ft_itoa.c
+++ level_4/ft_itoa/ft_itoa.c
@@ -2,45 +2,48 @@
 
 char	*ft_itoa(int nbr)
 {
-	  int	n = nbr;
-	  int	len = 0;
-	  char * result;
+	long	n = nbr;
+	int	len = 0;
+	char * result;
 	
-	  if (nbr <= 0)
-		  len++;
-	  while (n)
-	  {
-		  n = n / 10;
-		  len++;
-	  }
-	  result = malloc(sizeof(char) * (len + 1));
-	  if (!result)
-		  return (NULL);
-	  result[len] = '\0';
-	  if (nbr == 0)
-	  {
-		  result[0] = '0';
-		  return (result);
-	  }
-	  if (nbr < 0)
-	  {
-		  result[0] = '-';
-		  nbr = -nbr;
-	  }
-	  int	i = len - 1;
-	  while (nbr)
-	  {
-		  result[i] = nbr % 10 + '0';
-		  nbr = nbr / 10;
-		  i--;
-	  }
-	  return (result);
-  }
+	if (nbr <= 0)
+		len++;
+	while (n)
+	{
+		n = n / 10;
+		len++;
+	}
+	result = malloc(sizeof(char) * (len + 1));
+	if (!result)
+		return (NULL);
+	result[len] = '\0';
+	if (nbr == 0)
+	{
+		result[0] = '0';
+		return (result);
+	}
+	if (nbr < 0)
+	{
+		result[0] = '-';
+		n = -(long)nbr;
+	}
+	else
+		n = nbr;
+	int	i = len - 1;
+	while (n)
+	{
+		result[i] = n % 10 + '0';
+		n = n / 10;
+		i--;
+
+	}
+	return (result);
+}
 
 /*#include <stdio.h>
+#include <limits.h>
 
 int	main(void)
 {
-	int	n = -21;
-	printf("%s\n", ft_itoa(n));
+	printf("%s\n", ft_itoa(INT_MAX));
 }*/
