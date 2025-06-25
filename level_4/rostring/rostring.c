a53d8017fd3a0a1cb35ecf1f8de7abf10ed43c42
alicigar <aliciamomoko@gmail.com>
2025-06-25 10:43:55 +0200

    -

--- level_4/rostring/rostring.c
+++ level_4/rostring/rostring.c
@@ -1,73 +1,57 @@
 #include <unistd.h>
 
-int is_space(char c)
+void rostring(char *str)
 {
-    return (c == ' ' || c == '\t');
+    int i = 0;
+    int start = 0;
+    int end = 0;
+    
+    // 1. Saltar espacios iniciales
+    while (str[i] == ' ' || str[i] == '\t')
+        i++;
+    
+    // 2. Guardar inicio y fin de la primera palabra
+    start = i;
+    while (str[i] && str[i] != ' ' && str[i] != '\t')
+        i++;
+    end = i;
+    
+    // 3. Saltar espacios después de la primera palabra
+    while (str[i] == ' ' || str[i] == '\t')
+        i++;
+    
+    // 4. Imprimir el resto del string (sin espacios extra)
+    while (str[i])
+    {
+        if (str[i] == ' ' || str[i] == '\t')
+        {
+            write(1, " ", 1); // Imprimir solo un espacio
+            while (str[i] == ' ' || str[i] == '\t')
+                i++;
+        }
+        else
+        {
+            write(1, &str[i], 1);
+            i++;
+        }
+    }
+    
+    // 5. Imprimir espacio solo si hay más palabras (verificación implícita)
+    if (start != end && str[end] != '\0')
+        write(1, " ", 1);
+    
+    // 6. Imprimir la primera palabra al final
+    while (start < end)
+    {
+        write(1, &str[start], 1);
+        start++;
+    }
 }
 
 int main(int argc, char **argv)
 {
-    if (argc < 2)
-    {
-        write(1, "\n", 1);
-        return (0);
-    }
-
-    int i = 0;
-    int first_word_start = 0;
-    int first_word_end = 0;
-    int has_other_words = 0;
-
-    // 1. Saltar espacios iniciales
-    while (argv[1][i] && is_space(argv[1][i]))
-        i++;
-
-    // 2. Guardar inicio primera palabra
-    first_word_start = i;
-
-    // 3. Saltar primera palabra (caracteres no espacios)
-    while (argv[1][i] && !is_space(argv[1][i]))
-        i++;
-
-    // 4. Guardar fin primera palabra
-    first_word_end = i;
-
-    // 5. Saltar espacios después de primera palabra
-    while (argv[1][i] && is_space(argv[1][i]))
-        i++;
-
-    // 6. Verificar si hay más palabras después
-    has_other_words = (argv[1][i] != '\0');
-
-    // 7. Imprimir el resto del string (desde i hasta el final)
-    while (argv[1][i])
-    {
-        // Solo imprimir un espacio si es el primer espacio después de una palabra
-        if (is_space(argv[1][i]))
-        {
-            while (argv[1][i] && is_space(argv[1][i]))
-                i++;
-            if (argv[1][i])
-                write(1, " ", 1);
-        }
-        else
-        {
-            write(1, &argv[1][i], 1);
-            i++;
-        }
-    }
-
-    // 8. Imprimir espacio solo si había otras palabras después
-    if (has_other_words && first_word_start != first_word_end)
-        write(1, " ", 1);
-
-    // 9. Imprimir primera palabra al final
-    while (first_word_start < first_word_end)
-    {
-        write(1, &argv[1][first_word_start], 1);
-        first_word_start++;
-    }
-
+    if (argc == 2)
+        rostring(argv[1]);
     write(1, "\n", 1);
     return (0);
 }
