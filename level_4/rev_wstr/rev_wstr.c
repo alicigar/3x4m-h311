3ffab080818f1810981be79f9ec87a75b054d9e0
alicigar <aliciamomoko@gmail.com>
2025-06-26 14:17:19 +0200

    okidoki

--- level_4/rev_wstr/rev_wstr.c
+++ level_4/rev_wstr/rev_wstr.c
@@ -9,21 +9,23 @@ void	rev_wstr(char *str)
 	while (str[i])
 		i++;
 	i--;
-	while (i >= 0)
+	while (str[i])
 	{
-		while (i >= 0 && (str[i] == ' ' || str[i] == '\t'))
+		while (str[i] && (str[i] == 9 || str[i] == 32))
 			i--;
 		end = i;
-		while (i >= 0 && str[i] != ' ' && str[i] != '\t')
+		while (str[i] && str[i] != 9 && str[i] != 32)
 			i--;
-		start = i + 1;
+		i++;
+		start = i;
 		while (start <= end)
 		{
 			write(1, &str[start], 1);
 			start++;
 		}
-		if (i >= 0)
+		if (i > 0)
 			write(1, " ", 1);
+		i--;
 	}
 }
 
