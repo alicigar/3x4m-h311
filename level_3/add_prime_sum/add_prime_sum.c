1761074f6f24420d54ecf109eb12ae104e69e6f6
alicigar <aliciamomoko@gmail.com>
2025-06-29 23:51:04 +0200

    cool

---
+++ level_3/add_prime_sum/add_prime_sum.c
@@ -0,0 +1,70 @@
+#include <unistd.h>
+
+void	ft_putnbr(int n)
+{
+	if (n > 9)
+		ft_putnbr(n / 10);
+	write(1, &"0123456789"[n % 10], 1);
+}
+
+int	is_prime(int n)
+{
+	int	i;
+
+	if (n < 2)
+		return (0);
+	i = 2;
+	while (i <= n / 2)
+	{
+		if (n % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_atoi(char *str)
+{
+	int n = 0;
+	int neg = 1;
+	
+	while (*str <= 32)
+		str++;
+	while (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			neg = -neg;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		n = n * 10 + (*str - '0');
+		str++;
+	}
+	return (n * neg);
+}
+
+void	add_prime_sum(int n)
+{
+	int	sum;
+	int	i;
+
+	sum = 0;
+	i = 2;
+	while (i <= n)
+	{
+		if (is_prime(i))
+			sum += i;
+		i++;
+	}
+	ft_putnbr(sum);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+		add_prime_sum(ft_atoi(argv[1]));
+	else
+		write(1, "0", 1);
+	write(1, "\n", 1);
+}
