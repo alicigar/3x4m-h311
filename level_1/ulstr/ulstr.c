8efea4a844b853be2e3adff6a1b0f2124a405e30
alicigar <aliciamomoko@gmail.com>
2025-07-01 03:32:48 +0200

    ok.

--- level_1/ulstr/ulstr.c
+++ level_1/ulstr/ulstr.c
@@ -14,16 +14,14 @@
 
 void	ulstr(char *s)
 {
-	int	i;
-
-	i = 0;
+	int	i = 0;
 	while (s[i])
 	{
 		if (s[i] >= 'A' && s[i] <= 'Z')
 			s[i] = s[i] + 32;
 		else if (s[i] >= 'a' && s[i] <= 'z')
 			s[i] = s[i] - 32;
-		write (1, &s[i], 1);
+		write(1, &s[i], 1);
 		i++;
 	}
 }
@@ -32,6 +30,6 @@ int	main (int argc, char **argv)
 {
 	if (argc == 2)
 		ulstr(argv[1]);
-	write (1, "\n", 1);
+	write(1, "\n", 1);
 	return (0);
 }
