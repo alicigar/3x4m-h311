91875c1fc74077ad9454ed021d5d5d2f73788409
alicigar <aliciamomoko@gmail.com>
2025-07-01 16:27:58 +0200

    ok.

--- level_4/rostring/rostring.c
+++ level_4/rostring/rostring.c
@@ -5,35 +5,38 @@ void	rostring(char *str)
 	int	i = 0;
 	int	start = 0;
 	int	end = 0;
-	int	f = 0;  //0 si hay más palabras al recorrer el str, 1 si no o es solo 1 palabra el str//
-	
+	int	space = 0;
+
 	while (str[i] == 9 || str[i] == 32)
 		i++;
 	start = i;
-		
+
 	while (str[i] > 32)
 		i++;
 	end = i - 1;
-		
+
 	while (str[i] == 9 || str[i] == 32)
 		i++;
-
-	if (str[i] != 0)
-	{
-		f = 1;
-	}
-
 	while (str[i])
 	{
-		write(1, &str[i], 1);
+		if (str[i] == 9 || str[i] == 32)
+			space = 1;
+		else
+		{
+			if (space == 1)
+			{
+				write(1, " ", 1);
+				space = 0;
+			}
+			write(1, &str[i], 1);
+		}
 		i++;
 	}
-	
-	if (f)
+	while (str[i] == 0)
 	{
-		write(1, " ", 1);
+		write (1, " ", 1);
+		i++;
 	}
-	
 	while (start <= end)
 	{
 		write(1, &str[start], 1);
@@ -46,5 +49,5 @@ int	main(int argc, char **argv)
 	int	i = 1;
 	if (argc > i)
 		rostring(argv[1]);
-	write(1, "\n", 1);
+	write(1, "\n", 1);	
 }
