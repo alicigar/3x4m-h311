9efe32ae45948ddfc44d6574850eefa84cedb51d
alicigar <aliciamomoko@gmail.com>
2025-07-01 03:23:40 +0200

    ok.

--- level_1/rev_print/rev_print.c
+++ level_1/rev_print/rev_print.c
@@ -8,15 +8,15 @@ void    rev_print(char *s)
     i--;
     while (s[i])
     {
-        write (1, &s[i], 1);
+        write(1, &s[i], 1);
         i--;
     }
 }
 
 int main(int argc, char **argv)
 {
-    if(argc == 2)
+    if (argc == 2)
         rev_print(argv[1]);
-    write (1, "\n", 1);
+    write(1, "\n", 1);
     return (0);
 }
