fb28bb028206dcea91756a832dd7f9667fc8a644
alicigar <aliciamomoko@gmail.com>
2025-07-02 00:35:23 +0200

    ok.

--- level_3/lcm/lcm.c
+++ level_3/lcm/lcm.c
@@ -3,6 +3,8 @@ unsigned int    lcm(unsigned int a, unsigned int b)
         int temp;
         int r = 0;
 
+        if (a == 0 || b == 0)
+                return (0);
         r = a * b;
         while (b != 0)
         {
