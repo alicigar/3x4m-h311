744a273b6af7f4a2f7a2f89fa4af8a5ac647a19f
alicigar <aliciamomoko@gmail.com>
2025-07-01 03:25:52 +0200

    ok.

--- level_1/fizzbuzz/fizzbuzz.c
+++ level_1/fizzbuzz/fizzbuzz.c
@@ -4,25 +4,23 @@ void    ft_putnbr (int num)
 {
     if (num > 9)
         ft_putnbr(num / 10);
-    write (1, &"0123456789"[num % 10], 1);
+    write(1, &"0123456789"[num % 10], 1);
 }
 
 int main (void)
 {
-    int n;
-
-    n = 1;
+    int n = 1;
     while (n <= 100)
     {
         if ((n % 3 == 0) && (n % 5 == 0))
-            write (1, "fizzbuzz", 8);
+            write(1, "fizzbuzz", 8);
         else if (n % 5 == 0)
-            write (1, "buzz", 4);
+            write(1, "buzz", 4);
         else if (n % 3 == 0)
-            write (1, "fizz", 4);
+            write(1, "fizz", 4);
         else
             ft_putnbr(n);
         n++;
-        write (1, "\n", 1);
+        write(1, "\n", 1);
     }
 }
