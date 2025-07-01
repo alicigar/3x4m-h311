b45c3859bae0ee6c4bf85180c6c742a6c651e89d
alicigar <aliciamomoko@gmail.com>
2025-07-01 03:22:44 +0200

    ok.

--- level_1/rot_13/rot_13.c
+++ level_1/rot_13/rot_13.c
@@ -2,16 +2,14 @@
 
 void rot_13(char *str)
 {
-    int    i;
-
-    i = 0;
+    int    i = 0;
     while (str[i])
     {
         if ((str[i] >= 'A' && str[i] <= 'M') || (str[i] >= 'a' && str[i] <= 'm'))
             str[i] = str[i] + 13;
         else if((str[i] >= 'N' && str[i] <= 'Z') || str[i] >= 'n' && str[i] <= 'z'))
             str[i] = str[i] - 13;
-        write (1, &str[i], 1);
+        write(1, &str[i], 1);
         i++;
     }
 }
@@ -20,6 +18,6 @@ int main(int argc, char **argv)
 {
     if (argc == 2)
         rot_13(argv[1]);
-     write(1, "\n", 1);
+    write(1, "\n", 1);
     return (0);
 }
