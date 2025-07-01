a16e17368899ecb1ef45f7d7da7b8e3ac692b7b7
alicigar <aliciamomoko@gmail.com>
2025-07-01 04:33:05 +0200

    ok.

--- level_2/last_word/last_word.c
+++ level_2/last_word/last_word.c
@@ -14,9 +14,7 @@
 
 void	last_word(char *str)
 {
-	int	i;
-	
-	i = 0;
+	int	i = 0;
 	while (str[i])
 		i++;
 	i--;
@@ -27,7 +25,7 @@ void	last_word(char *str)
 	i++;
 	while (str[i] && str[i] > 32)
 	{
-		write (1, &str[i], 1);
+		write(1, &str[i], 1);
 		i++;
 	}
 	
@@ -37,6 +35,6 @@ int	main(int argc, char **argv)
 {
 	if (argc == 2)
 		last_word(argv[1]);
-	write (1, "\n", 1);
+	write(1, "\n", 1);
 	return (0);
 }
