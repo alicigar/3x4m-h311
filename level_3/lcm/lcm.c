9f67cf128b1d3987c281a47ba078257240d244df
alicigar <aliciamomoko@gmail.com>
2025-07-02 00:30:26 +0200

    ok.

---
+++ level_3/lcm/lcm.c
@@ -0,0 +1,21 @@
+unsigned int    lcm(unsigned int a, unsigned int b)
+{
+        int temp;
+        int r = 0;
+
+        r = a * b;
+        while (b != 0)
+        {
+                temp = b;
+                b = a % b;
+                a = temp;
+        }
+        return (r / a);
+}
+
+/*#include <stdio.h>
+
+int     main(void)
+{
+        printf("%d\n", lcm(3, 5));
+}
