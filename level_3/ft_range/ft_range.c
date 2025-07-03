e82251511beebd80e224e732e3523856fb6630cb
alicigar <aliciamomoko@gmail.com>
2025-07-03 02:52:14 +0200

    ok.

--- level_3/ft_range/ft_range.c
+++ level_3/ft_range/ft_range.c
@@ -1,31 +1,34 @@
+#include <stdlib.h>
+
 int	*ft_range(int start, int end)
 {
-	int i = 0;
-	int size = (end - start + 1);
-	int *nums = (int *) malloc(size * sizeof(int));
-	//if (!nums)
-	//	return (NULL);
-	//podemos omitir estas dos ultimas lineas
-	while (start <= end)
+	int	i = 0;
+	int	*array;
+	int	size;
+	
+	if (start < end)
+		size = end - start + 1;
+	else
+		size = start - end + 1;
+	array = malloc(size * sizeof(int));
+	
+	if (start < end)
 	{
-		nums[i] = start;
-		i++;
-		start++;
+		while (start <= end)
+		{
+			array[i] = start;
+			i++;
+			start++;
+		}
 	}
-	return (nums);
+	else
+	{
+		while (start >= end)
+		{
+			array[i] = start;
+			i++;
+			start--;
+		}
+	}
+	return (array);
 }
-
-/*#include <stdio.h>
-
-int	main()
-{
-	int	*arr = ft_rrange(0, 4);
-	int	size = 0;
-
-	while (size < 5)
-	{
-		printf("%i ", arr[size]);
-		size++;
-	}
-	free(arr);
-}*/
