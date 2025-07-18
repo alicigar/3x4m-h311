9a80b73c818a828da669d3cbcbc378984733ede3
alicigar <aliciamomoko@gmail.com>
2025-07-18 03:36:22 +0200

    ok.

--- level_2/reverse_bits/reverse_bits.c
+++ level_2/reverse_bits/reverse_bits.c
@@ -1,12 +1,12 @@
 unsigned char	reverse_bits(unsigned char octet)
 {
-	unsigned int	i = 8;
-	unsigned char	r = 0;
+	unsigned int	i = 7;
+	unsigned char	r;
 	
-	while (i--)
+	while (i >= 0)
 	{
-		r = (r << 1) | (octet & 1);
-		octet = octet >> 1;
+		r = (r << 1) | ((octet & i) & 1);
+		i--;
 	}
 	return (r);
 }
